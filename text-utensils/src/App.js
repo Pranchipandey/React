00cf6082fc54d435928502a2c233c454c2060820
Pranchipandey <pranchi.pandey_cs19@gla.ac.in>
2022-06-16 14:10:47 +0530

    react-app

--- text-utensils/src/App.js
+++ text-utensils/src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-//import About from './components/About.js';
+import About from './components/About.js';
 import React ,{useState} from 'react';
 import Navbar from './components/Navbar';
 import TextForm  from './components/TextForm';
 
 import Alert from './components/Alert';
-//import {
- // BrowserRouter as Router,
-//  Routes,
-  //Route,
- // Link
-//} from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+ Routes,
+  Route,
+  
+} from 'react-router-dom';
 function App() {
   const [mode,setMode] = useState('light');
   const [alert, setAlert] = useState(null);
@@ -39,23 +39,20 @@ function App() {
   }
   return (
 <>
-{/*<Router>*/}
-<Navbar  mode={mode} toggleMode={toggleMode}/>
+<Router>
+<Navbar title="TextUtils" aboutText="about" mode={mode} toggleMode={toggleMode}/>
  <Alert alert={alert}/>
-{/*<div className="container">
+<div className="container">
         <Routes>
-            <Route path="/about" element={<About />}>
+            <Route path="/about" element={<About mode={mode} />}>
             </Route>
-            <Route path="/" element={<TextForm heading="Enter text to analyze"  mode={mode} showAlert={showAlert} />}>
+            <Route path="/" element={<TextForm heading="Try TextUtils - word counter, character counter, remove extra spaces"  mode={mode} showAlert={showAlert} />}>
             </Route>
           </Routes>
         </div>
 
-</Router>*/}
-<div className="container">
-<TextForm heading="Enter text to analyze"  mode={mode} showAlert={showAlert} />
+</Router>
 
-</div>
 </>
   );
 }
